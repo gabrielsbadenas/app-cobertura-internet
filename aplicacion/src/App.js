6a1bc921826fc4b6dd533251bd32cc002e6035e4
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2022-08-08 16:52:08 -0300

    crear una nueva funcion de react

--- aplicacion/src/App.js
+++ aplicacion/src/App.js
@@ -1,8 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './models/User'
+let index=0
+function Usuario(){
+ // constructor(){}
+    return (<p>hola mundo</p>)
+}
 function UserView(){
-  let user ={user:User.users[1]}// new User()
+  let user ={user:User.users[index]}// new User()
   console.log(User.users)
   //to do: sacar uno de los user (cambiar user.user por user solo)/
   return (  
@@ -21,25 +26,16 @@ function UserView(){
   )
 }
 function App() {
-  let user ={user:User.users[0]}// new User()
   console.log(User.users)
   //to do: sacar uno de los user (cambiar user.user por user solo)/
   return (  
     <div className="App">
     <UserView></UserView>
-    <br></br>
-      <p font="black">nombre: {user.user.nombres.primer}</p>
-      <p font="black">apellido: {user.user.nombres.ultimo}</p>
-      <p font="black">tipo de documento: {user.user.documento.tipo}</p>
-      <p font="black">numero de documento: {user.user.documento.numero}</p>
-      <p font="black">email: {user.user.email.sub+user.user.email.at+user.user.email.domain}</p>
-      <p font="black">telefono: {user.user.phone.country.code+user.user.phone.area.number+user.user.phone.number}</p>
-      <p>domicilio:
-{user.user.domicilio.provincia}+{user.user.domicilio.localidad}+{user.user.domicilio.codigopostal}+{user.user.domicilio.calle}+{user.user.domicilio.numero}+{user.user.domicilio.puerta}
-
-      </p>
+    <Usuario></Usuario>
     </div>
+
   );
 }
 
 export default App;
+//TODO HACER QUE EL NUMERO DE TELEFONO SEA UN INT SIN CODIGO DE PAIS NI NADA
