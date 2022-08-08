07cee42eafcba4faf1edb0c14e55bc0ce03ac889
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2022-08-08 15:00:57 -0300

    agregar view de la informacion de usuario

--- aplicacion/src/App.js
+++ aplicacion/src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 import User from './models/User'
 function App() {
   let user =User// new User()
+  console.log(user.user)
   return (
     <div className="App">
-      <p>user.stringify()</p>
+      <p font="black">nombre: {user.user.nombres.primer}</p>
+      <p font="black">apellido: {user.user.nombres.ultimo}</p>
+      <p font="black">tipo de documento: {user.user.documento.tipo}</p>
+      <p font="black">numero de documento: {user.user.documento.numero}</p>
+      <p font="black">email: {user.user.email.sub+user.user.email.at+user.user.email.domain}</p>
+      <p font="black">telefono: {user.user.phone.country.code+user.user.phone.area.number+user.user.phone.number}</p>
     </div>
   );
 }
