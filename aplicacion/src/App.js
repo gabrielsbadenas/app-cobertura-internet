fe01cc148315f5c7a0467257f529d833c6acdb6e
gabrielsbadenas <gabrielsbadenas@gmail.com>
2022-08-12 16:00:20 -0300

    agregar campo de coordenadas a la view

--- aplicacion/src/App.js
+++ aplicacion/src/App.js
@@ -62,7 +62,8 @@ function UserView(props){
       <p font="black">telefono: {''+user.user.phone.country.code+user.user.phone.area.number+user.user.phone.number}</p>
       <p>domicilio:
 {user.user.domicilio.provincia}+{user.user.domicilio.localidad}+{user.user.domicilio.codigopostal}+{user.user.domicilio.calle}+{user.user.domicilio.numero}+{user.user.domicilio.puerta}
-
+<p font='black'>coordenadas: 0 0</p>
+#todo agregar campo de coordenadas al user
       </p>
     </div>
   )
