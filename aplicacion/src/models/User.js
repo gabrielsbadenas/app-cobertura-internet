6c651dc2af925f10a95546ac7d04178179c89279
gabrielsbadenas <gabrielsbadenas@gmail.com>
2022-08-17 22:11:26 -0300

    autoformater de visual studio code

--- aplicacion/src/models/User.js
+++ aplicacion/src/models/User.js
@@ -1,4 +1,4 @@
-class User{
+class User {
 	constructor(
 		primer,
 		ultimo,
@@ -15,39 +15,39 @@ class User{
 		country,
 		area,
 		number
-		){
-		this.nombres={primer,ultimo}
-		this.documento={tipo,numero:doc}
-		this.email={sub:sub,at:"@",domain:domain}
-		this.phone={country,area,number}
-		this.domicilio={
-			provincia:provincia,
-			calle:calle,
-			numero:numero,
-			puerta:puerta,
-			localidad:localidad,
-			codigopostal:codigopostal
+	) {
+		this.nombres = { primer, ultimo }
+		this.documento = { tipo, numero: doc }
+		this.email = { sub: sub, at: "@", domain: domain }
+		this.phone = { country, area, number }
+		this.domicilio = {
+			provincia: provincia,
+			calle: calle,
+			numero: numero,
+			puerta: puerta,
+			localidad: localidad,
+			codigopostal: codigopostal
 		}
 		//this.email={sub:sub,at:'@',domain:domain},
 		//this.phone={country,area,number}
 	}
 }
-const gabriel={
+const gabriel = {
 	nombres: {
 		primer: 'gabriel',
 		ultimo: 'badenas'
 	},
-	documento:{
-		tipo:'dni',
-		numero:'12345678'
+	documento: {
+		tipo: 'dni',
+		numero: '12345678'
 	},
-	domicilio:{
-		provincia:'caba',
-		calle:'rivadavia',
-		numero:1,
-		puerta:'pb',
-		localidad:'comuna 11',
-		codigopostal:'1416'
+	domicilio: {
+		provincia: 'caba',
+		calle: 'rivadavia',
+		numero: 1,
+		puerta: 'pb',
+		localidad: 'comuna 11',
+		codigopostal: '1416'
 	},
 	email: {
 		sub: 'gabrielsbadenas',
@@ -62,24 +62,24 @@ const gabriel={
 		area: {
 			number: 11
 		},
-		number:12345678
+		number: 12345678
 	}
 }
-	const fer = new User(
-		'wiki',
-		'sanchez',
-		'dni',
-		88888888,
-		'buenos aires',
-		'25 de mayo',
-		420,'1b','tigre'
-		,1234,'wiki-san'
-		,'wiki.com','54','11'
-		,'12341234'
-		)
+const fer = new User(
+	'wiki',
+	'sanchez',
+	'dni',
+	88888888,
+	'buenos aires',
+	'25 de mayo',
+	420, '1b', 'tigre'
+	, 1234, 'wiki-san'
+	, 'wiki.com', '54', '11'
+	, '12341234'
+)
 
-const users=[
-	fer,gabriel
+const users = [
+	fer, gabriel
 ]
-export default {users}
+export default { users }
 //todo: hacerlo pero con class y Usernstructor
