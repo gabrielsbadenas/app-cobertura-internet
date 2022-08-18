8fd7f857d78d45da75a9beee0079ff43030ef678
gabrielsbadenas <gabrielsbadenas@gmail.com>
2022-08-18 18:04:36 -0300

    compare coordinates

---
+++ aplicacion/src/models/CompareCoordinates.js
@@ -0,0 +1,27 @@
+class coordinate {
+    constructor(x0, y1, x2, y3) {
+        this.x0 = x0
+        this.y1 = y1
+        this.x2 = x2
+        this.y3 = y3
+    }
+}
+function compareCoordinates(userLocation, coverageZone) {
+
+    const a = userLocation
+    const b = coverageZone
+    const c = (
+        (
+            (a.x0 < b.x0 < a.x2)
+            ||
+            (a.x0 < b.x2 < a.x2))
+        &&
+        ((
+            a.y1 < b.y1 < a.y3
+        )
+            ||
+            (
+                a.y1 < b.y3 < a.y3
+            ))
+    )
+}
