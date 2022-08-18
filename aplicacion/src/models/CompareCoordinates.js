938deb3a2d85a85cc07b520e8300faaa09b92bcb
gabrielsbadenas <gabrielsbadenas@gmail.com>
2022-08-18 18:07:18 -0300

    test case 1 y poner mayuscula a Coordinate

--- aplicacion/src/models/CompareCoordinates.js
+++ aplicacion/src/models/CompareCoordinates.js
@@ -1,4 +1,4 @@
-class coordinate {
+class Coordinate {
     constructor(x0, y1, x2, y3) {
         this.x0 = x0
         this.y1 = y1
@@ -6,6 +6,7 @@ class coordinate {
         this.y3 = y3
     }
 }
+const barrio = new Coordinate(-34,-58,-35,-59)
 function compareCoordinates(userLocation, coverageZone) {
 
     const a = userLocation
