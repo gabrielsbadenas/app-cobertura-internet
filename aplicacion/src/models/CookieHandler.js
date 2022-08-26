6fb37b8b25d8ef53324276f467f137db28964445
gabrielsbadenas <gabrielsbadenas@gmail.com>
2022-08-26 15:15:47 -0300

    agregarle comillas al name del cookie handler

--- aplicacion/src/models/CookieHandler.js
+++ aplicacion/src/models/CookieHandler.js
@@ -1,5 +1,5 @@
 export default function CookieHandle(){
-    let jsonUser = "{nombre:name}"
+    let jsonUser = "{nombre:'name'}"
     //document.cookie = "docs=https://www.w3schools.com/js/js_cookies.asp"
     document.cookie = "user="+jsonUser
 }
