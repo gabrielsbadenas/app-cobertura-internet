b9e65e39f5ff2723a44421f07b7a194140494417
gabrielsbadenas <gabrielsbadenas@gmail.com>
2022-08-30 16:11:43 -0300

    formatear el render del userForm

--- aplicacion/src/models/UserForm.js
+++ aplicacion/src/models/UserForm.js
@@ -11,31 +11,38 @@ class UserForm extends React.Component {
                 '', '', '', '', '')
         }
     }
-    handle1(){}
-    handle2(){}
-    handle3(){}
-    handle4(){}
-    handle5(){}
-    handle6(){}
-    handle7(){}
+    handle1() { }
+    handle2() { }
+    handle3() { }
+    handle4() { }
+    handle5() { }
+    handle6() { }
+    handle7() { }
 
-    handle8(){}
-    handle9(){}
-    handle10(){}
-    handle11(){}
-    handle12(){}
-    handle13(){}
-    handle14(){}
+    handle8() { }
+    handle9() { }
+    handle10() { }
+    handle11() { }
+    handle12() { }
+    handle13() { }
+    handle14() { }
 
-    handle15(){}
+    handle15() { }
 
     render() {
         console.log('userform12', this.state)
         return (
-            <p>hola { 'x:'+Math.sqrt(160*144)}{' y:'+Math.sqrt(180)} mundo
-            x: {'-34.6227364'}
-            y: {-58.1882655}
-            </p>
+            <div>
+                <h1>
+                coordenadas actuales
+                </h1>
+                <p>
+                    hola {'x:' + Math.sqrt(160 * 144)}
+                    {' y:' + Math.sqrt(180)} mundo
+                    x: {'-34.6227364'}
+                    y: {-58.1882655}
+                </p>
+            </div>
         )
     }
 }
