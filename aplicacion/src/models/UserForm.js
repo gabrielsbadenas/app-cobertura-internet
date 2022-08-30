8d2ce9eb4d2655635e3b2c72671225bef04e5dca
gabrielsbadenas <gabrielsbadenas@gmail.com>
2022-08-30 15:28:46 -0300

    crear 15 handlers

--- aplicacion/src/models/UserForm.js
+++ aplicacion/src/models/UserForm.js
@@ -11,6 +11,24 @@ class UserForm extends React.Component {
                 '', '', '', '', '')
         }
     }
+    handle1(){}
+    handle2(){}
+    handle3(){}
+    handle4(){}
+    handle5(){}
+    handle6(){}
+    handle7(){}
+
+    handle8(){}
+    handle9(){}
+    handle10(){}
+    handle11(){}
+    handle12(){}
+    handle13(){}
+    handle14(){}
+
+    handle15(){}
+
     render() {
         console.log('userform12', this.state)
         return (
