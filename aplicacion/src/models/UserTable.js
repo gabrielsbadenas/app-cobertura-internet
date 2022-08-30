f84ee46c3b2449635fe0083f1585e50010933565
gabrielsbadenas <gabrielsbadenas@gmail.com>
2022-08-30 14:45:27 -0300

    fill userTable

--- aplicacion/src/models/UserTable.js
+++ aplicacion/src/models/UserTable.js
@@ -1,5 +1,37 @@
-export default class UserTable{
-    constructor(){
-
+export default class UserTable {
+    constructor(
+        primer,
+        ultimo,
+        tipo,
+        doc,
+        provincia,
+        calle,
+        numero,
+        puerta,
+        localidad,
+        codigopostal,
+        sub,
+        domain,
+        country,
+        area,
+        number
+    ) {
+        this.user = {
+            primer,
+            ultimo,
+            tipo,
+            doc,
+            provincia,
+            calle,
+            numero,
+            puerta,
+            localidad,
+            codigopostal,
+            sub,
+            domain,
+            country,
+            area,
+            number
+        }
     }
 }
